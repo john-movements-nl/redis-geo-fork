7de2015ca3261ca01ce1b3f9375920a11c8d9727
Darren Reid <darren.j.reid@gmail.com>
2016-10-06 20:11:44 +1100

    Fix create service check

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ docker push $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region $AWS_DEFAULT_REGION > /dev/null # Create a new task revision
 TASK_REVISION=$(aws ecs describe-task-definition --task-definition $ECS_TASK --region $AWS_DEFAULT_REGION | jq '.taskDefinition.revision') #get latest revision
 CREATE_NEW_SERVICE=$(aws ecs list-services --region $AWS_DEFAULT_REGION --cluster $AWS_ECS_CLUSTER_NAME | jq '.serviceArns' | jq 'contains(["arn:aws:ecs:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_NUMBER:service/$ECS_SERVICE"])')
-if [ "$CREATE_NEW_SERVICE" = "true" ]; then
+if [ "$CREATE_NEW_SERVICE" == "true" ]; then
     echo "ECS Service already exists"
     aws ecs update-service --cluster $AWS_ECS_CLUSTER_NAME --service $ECS_SERVICE --task-definition "$ECS_TASK:$TASK_REVISION" --desired-count 1 --region $AWS_DEFAULT_REGION > /dev/null #update service with latest task revision
 else
