feada7b9fccd3f4b1c5a4c3f690f54846f05547d
Darren Reid <darren.j.reid@gmail.com>
2016-10-13 12:01:01 +1100

    Fix docker build path arg

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 source ../set-envs.sh
-# navigate to directory with Dockerfile
-cd ..
+
 #AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
-docker build -t $IMAGE_NAME .
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
