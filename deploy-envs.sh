383049a476776e34d8f2474e14b1a98ae2990a8a
John Baltussen <john@movements.nl>
2018-01-05 12:18:24 +0100

    removed AWS credentials, becaus public repo

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -11,6 +11,3 @@ export AWS_VIRTUAL_HOST=adduranceV2.movements.nl
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 # optional: SERVICESTACK_LICENSE
-export AWS_ACCOUNT_NUMBER=126533691392
-export AWS_ACCESS_KEY_ID=AKIAIXQLE3WNDVC33LWA
-export AWS_SECRET_ACCESS_KEY=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
